eed872d1cfc0288ce00900a067fb2b3d3fb1df40
Andrew <andrewscottpowers@gmail.com>
2018-07-31 16:11:40 -0700

    just added my js and renamed to from main to app in the html head

---
+++ project-directory/js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var userName = prompt("whats your name?")
+console.log("the user name is: " + userName);
+alert("Hey " + userName +" welcome to the M.O.R.E. House, I just have a few questions to ask you");
+
+var building = prompt("Do you enjoy buidling solutions?");
+console.log("the user said " + building + " to being a Maker");
+
+var afrofuture = prompt("Have you been inspired to create an Afrofuture?");
+console.log(userName +"said"+ afrofuture + "having been inspired to create an Afrofuture ");
+
+var mathmatics = prompt("Do you see how tech, art, and music fall in the same mathmatics?");
+console.log("the user said" + building +"to being a Maker");
+
+var building = prompt("is your goal to make sci-fi just science?");
+console.log("the user said" + building +"to being a Maker");
+
+
+
+    
